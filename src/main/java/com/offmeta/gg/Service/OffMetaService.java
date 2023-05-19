23dc3b1c0f79101ed8f650b033efc6d5010b2132
timparka <pimtarka@gmail.com>
2023-05-19 12:52:24 -0400

    seperated OffMetaServices into different methods

--- src/main/java/com/offmeta/gg/Service/OffMetaService.java
+++ src/main/java/com/offmeta/gg/Service/OffMetaService.java
@@ -19,14 +19,28 @@ public class OffMetaService {
 
     public OffMetaDTO getOffMetaPick(String lane) {
         List<ParticipantEntity> participants = participantRepository.findAll();
+        List<ParticipantEntity> laneParticipants = filterParticipantsByLane(participants, lane);
+        int totalMatches = calculateTotalMatches(laneParticipants);
+        Map<String, ChampionStats> championStatsMap = generateChampionStats(laneParticipants);
+        calculateRatesForAllChampions(championStatsMap, totalMatches);
+        List<ChampionStats> championStatsList = sortChampionStatsList(championStatsMap);
 
-        List<ParticipantEntity> laneParticipants = participants.stream()
+        ChampionStats bestChampion = championStatsList.get(0);
+
+        return buildOffMetaDTO(bestChampion, lane);
+    }
+
+    private List<ParticipantEntity> filterParticipantsByLane(List<ParticipantEntity> participants, String lane) {
+        return participants.stream()
                 .filter(participant -> lane.equalsIgnoreCase(participant.getRole()))
                 .collect(Collectors.toList());
+    }
 
-        //need to divide by 2 as there are 2 of the same laners in a game
-        int totalMatches = laneParticipants.size() / 2;
+    private int calculateTotalMatches(List<ParticipantEntity> laneParticipants) {
+        return laneParticipants.size() / 2;
+    }
 
+    private Map<String, ChampionStats> generateChampionStats(List<ParticipantEntity> laneParticipants) {
         Map<String, ChampionStats> championStatsMap = new HashMap<>();
         for (ParticipantEntity participant : laneParticipants) {
             championStatsMap.putIfAbsent(participant.getChampionName(), new ChampionStats(participant.getChampionName()));
@@ -38,25 +52,29 @@ public class OffMetaService {
             stats.addItemBuild(participant.getItems());
             stats.addSummonerSpells(participant.getSummonerSpell1(), participant.getSummonerSpell2());
         }
+        return championStatsMap;
+    }
 
+    private void calculateRatesForAllChampions(Map<String, ChampionStats> championStatsMap, int totalMatches) {
         for (ChampionStats stats : championStatsMap.values()) {
             stats.calculateWinRateAndPickRate(totalMatches);
         }
+    }
 
+    private List<ChampionStats> sortChampionStatsList(Map<String, ChampionStats> championStatsMap) {
         List<ChampionStats> championStatsList = new ArrayList<>(championStatsMap.values());
         championStatsList.sort(Comparator.comparingDouble(ChampionStats::getPickRate)
                 .thenComparing(ChampionStats::getWinRate, Comparator.reverseOrder()));
+        return championStatsList;
+    }
 
-        ChampionStats bestChampion = championStatsList.get(0);
-
+    private OffMetaDTO buildOffMetaDTO(ChampionStats bestChampion, String lane) {
         OffMetaDTO offMetaPick = new OffMetaDTO();
         offMetaPick.setChampionName(bestChampion.getChampionName());
         offMetaPick.setItems(bestChampion.getMostCommonItemBuild());
         offMetaPick.setSummonerSpell1(bestChampion.getMostCommonSummonerSpell1());
         offMetaPick.setSummonerSpell2(bestChampion.getMostCommonSummonerSpell2());
         offMetaPick.setRole(lane);
-
         return offMetaPick;
     }
-
 }
