2eaaa44af27e7e51c1b2644f5068e1504a507f1d
Tim Park <pimtarka@gmail.com>
2023-09-16 15:15:46 -0400

    cors

--- src/main/java/com/offmeta/gg/Controller/UserController.java
+++ src/main/java/com/offmeta/gg/Controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://timparkaoffmetafe.s3-website-us-east-1.amazonaws.com")
+@CrossOrigin(origins = "*")
 public class UserController {
 
 //    @Autowired
